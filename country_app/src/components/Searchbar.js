8e63358d62d81c71026421df4cd9acbe245ca46d
rohitnayal12 <rohitnayalkld@gmail.com>
2023-12-11 11:38:37 +0530

    updated

--- country_app/src/components/Searchbar.js
+++ country_app/src/components/Searchbar.js
@@ -1,56 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import CardList from './CardList';
-import axios from 'axios'
+import axios from 'axios';
 
 const Searchbar = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [countries, setCountries] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      if (searchTerm === '') {
+        const res = await axios.get(`https://restcountries.com/v3.1/all`);
+        setCountries(res.data);
+      } else {
+        const res = await axios.get(
+          `https://restcountries.com/v3.1/currency/${searchTerm}`
+        );
+        setCountries(res.data);
+      }
+    } catch (error) {
+      setCountries([]);
+      console.error('Error fetching data:', error);
+    }
+  };
 
-
-
-    const fetchData = async () => {
-        try {
-          if (searchTerm == "") {
-            const res = await axios.get(
-              `https://restcountries.com/v3.1/all`
-            );
-            setCountries(res.data);
-          } else {
-            const res = await axios.get(
-              `https://restcountries.com/v3.1/currency/${searchTerm}`
-            );
-            setCountries(res.data);
-          }
-        } catch (error) {
-            setCountries([])
-          console.error("Error fetching data:", error);  
-
-        }
-      };
-      console.log(searchTerm)
-      console.log(countries)
-  
-    useEffect(() => {
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
       fetchData();
-    }, [searchTerm]);
-  
-    return (
-      <div style={{width:"100%"}} className="search-container">
-        <input
-          type="text"
-          placeholder="Enter text "
-          class="search-input"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <div style={{width:"100%", 
-     }} >
+    }, 2000);
+
+    return () => {
+      clearTimeout(debounceTimer);
+    };
+  }, [searchTerm]);
+
+  return (
+    <div style={{ width: '100%' }} className="search-container">
+      <input
+        type="text"
+        placeholder="Enter text "
+        className="search-input"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div style={{ width: '100%',marginTop:"40px" }}>
         <CardList countries={countries} />
       </div>
-        
-      </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
